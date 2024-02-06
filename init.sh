4a19bd13233952bc3901ff48ffd4a6557dd47921
Geek <geekgames74.fr@gmail.com>
2024-02-06 19:55:06 +0100

    init.sh

---
+++ init.sh
@@ -0,0 +1,7 @@
+sudo apt-get -y install python3
+sudo apt-get -y install pip
+sudo apt-get -y install python3-venv
+screen -S gamma
+python3 -m venv .venv
+source .venv/bin/activate
+python3 -m pip install -r requirements.txt
