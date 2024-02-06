dbdc3f112a9df8a15d7b52553cd45206220a251d
Geek <geekgames74.fr@gmail.com>
2024-02-06 20:51:26 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 sudo apt-get -y install python3
 sudo apt-get -y install pip
 sudo apt-get -y install python3-venv
-screen -S gamma
 python3 -m venv .venv
 source .venv/bin/activate
+screen -S gamma
 python3 -m pip install -r requirements.txt
