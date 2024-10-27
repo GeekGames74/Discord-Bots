bef4d723f8a315cf84497f14b071774c52225343
Geek <geekgames74.fr@gmail.com>
2024-10-27 02:09:25 +0100

    Testing script
    small update to the testing script
    can now support a filename parameter

--- Modules/test.bat
+++ Modules/test.bat
@@ -7,12 +7,19 @@ set PROJECT_ROOT=%~dp0\..
 :: Change directory to the project root
 cd /d %PROJECT_ROOT%
 
-:: Loop through all .py files in the Modules directory
-for %%f in (Modules\*.py) do (
-    :: Get the filename without extension
-    set FILE_NAME=%%~nf
+:: Check if a filename was provided as a command-line argument
+if "%~1"=="" (
+    :: Ask for a specific file name to run, without .py extension
+    set /p FILE_TO_RUN="Enter filename to run (without extension): "
+) else (
+    :: Use the provided argument as the filename
+    set FILE_TO_RUN=%~1
+)
 
-    :: Skip __init__.py
+GOTO:MAIN
+
+:Run
+    set FILE_NAME=%~1
     if /i not "!FILE_NAME!"=="__init__" (
         echo ====================================================================================================
         echo RUNNING !FILE_NAME! & echo:
@@ -22,6 +29,18 @@ for %%f in (Modules\*.py) do (
 
         echo: & echo EXITING !FILE_NAME!
         echo ====================================================================================================
+    )
+EXIT /B 0
+
+:MAIN
+
+:: Loop through all .py files in the Modules directory
+for %%f in (Modules\*.py) do (
+    SET FILE_NAME=%%~nf
+    if not defined FILE_TO_RUN (
+        call:Run "!FILE_NAME!"
+    ) else if /i "!FILE_NAME!"=="!FILE_TO_RUN!" (
+        call:Run "!FILE_NAME!"
     )
 )
 
